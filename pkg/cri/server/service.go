23b5a10206fce559766553c6fd0421c3e570b211
wllenyj <wllenyj@linux.alibaba.com>
2021-08-30 03:13:24 +0800

    [CRI] Pass criService Config by pointer instead of value
    Pass criService Config by pointer instead of value to
    - share configuration among multiple clients
    - avoid unnecessary memory copy
    
    Signed-off-by: wanglei <wllenyj@linux.alibaba.com>

--- pkg/cri/server/service.go
+++ pkg/cri/server/service.go
@@ -76,7 +76,7 @@ type CRIService interface {
 // criService implements CRIService.
 type criService struct {
 	// config contains all configurations.
-	config criconfig.Config
+	config *criconfig.Config
 	// imageFSPath is the path to image filesystem.
 	imageFSPath string
 	// os is an interface for all required os operations.
@@ -121,7 +121,7 @@ type criService struct {
 }
 
 // NewCRIService returns a new instance of CRIService
-func NewCRIService(config criconfig.Config, client *containerd.Client) (CRIService, error) {
+func NewCRIService(config *criconfig.Config, client *containerd.Client) (CRIService, error) {
 	var err error
 	labels := label.NewStore()
 	c := &criService{
@@ -176,7 +176,7 @@ func NewCRIService(config criconfig.Config, client *containerd.Client) (CRIServi
 	}
 
 	// Preload base OCI specs
-	c.baseOCISpecs, err = loadBaseOCISpecs(&config)
+	c.baseOCISpecs, err = loadBaseOCISpecs(config)
 	if err != nil {
 		return nil, err
 	}
